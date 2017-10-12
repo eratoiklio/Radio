00f61fc3e65187aeb283d4e0b2c1d2b833d929e7
eratoiklio <eratoiklio@gmail.com>
2017-10-12 11:14:05 +0200

    station details visibility

--- js/main.js
+++ js/main.js
@@ -2,14 +2,19 @@ require('../scss/style.scss');
 
 document.addEventListener('DOMContentLoaded', function() {
     console.log("script loaded");
-    const stations = document.querySelectorAll(".particularStation");
-    stations.forEach(function(element, index)
-                     {
-                    element.onclick=function()
-                    {
-                    //  this.style.visibility = "hidden";
-                    console.log("hej");
-                    }
-      });
+    const stations = document.querySelectorAll(".shortcut");
+    let currentStation;
+    stations.forEach(function(element, index) {
+        element.onclick = function() {
+
+            console.log(this.parentNode);
+            if (currentStation != undefined) {
+                currentStation.classList.add("hidden");
+            }
+            currentStation = this.parentNode.querySelector(".extended");
+            currentStation.classList.remove("hidden");
+
+        }
+    });
 
 });
