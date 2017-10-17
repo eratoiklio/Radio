ec13ffa010bc7f6f67e5a5811706b20ef1b576a0
eratoiklio <eratoiklio@gmail.com>
2017-10-17 20:19:37 +0200

    close station onclick

--- js/main.js
+++ js/main.js
@@ -1,9 +1,51 @@
 require('../scss/style.scss');
 import stationsName from "../stationsName.js"
+// import url from "./url.js"
 $(() => {
-    const epsilon=0.000001;
-        const tab = $(stationsName);
-        var audio = new Audio();
+// var stationsName;
+    // function createCORSRequest(method, url) {
+    //   var xhr = new XMLHttpRequest();
+    //   if ("withCredentials" in xhr) {
+    //     // XHR for Chrome/Firefox/Opera/Safari.
+    //     xhr.open(method, url, true);
+    //
+    //   } else if (typeof XDomainRequest != "undefined") {
+    //     // XDomainRequest for IE.
+    //     xhr = new XDomainRequest();
+    //     xhr.open(method, url);
+    //   } else {
+    //     // CORS not supported.
+    //     xhr = null;
+    //   }
+    //   return xhr;
+    // }
+    //
+    // function makeCorsRequest() {
+    //   // This is a sample server that supports CORS.
+    //
+    //   var xhr = createCORSRequest('GET', url);
+    //   if (!xhr) {
+    //     alert('CORS not supported');
+    //     return;
+    //   }
+    //
+    //   // Response handlers.
+    //   xhr.onload = function() {
+    //     var text = xhr.responseText;
+    //     stationsName = JSON.parse(text);
+    //
+    //   };
+    //
+    //   xhr.onerror = function() {
+    //     alert('Woops, there was an error making the request.');
+    //   };
+    //
+    //   xhr.send();
+    // }
+    // makeCorsRequest();
+    const epsilon = 0.000001;
+    const tab = $(stationsName);
+    var audio = new Audio();
     $(tab).each(function(index, element) {
 
         const newParticStat = $("<div class='particularStation'>");
@@ -11,18 +53,17 @@ $(() => {
         const newMinus = $("<div class='minus'>");
 
         const newStationImage = $("<div class='stationImage'>")
-        if(element.image.url!=""){
-            newStationImage.css( "background", "url("+element.image.url+")" );
-        }
-        else {
-            newStationImage.css( "background", "url(./img/default.jpg)" );
+        if (element.image.url != "") {
+            newStationImage.css("background", "url(" + element.image.url + ")");
+        } else {
+            newStationImage.css("background", "url(./img/default.jpg)");
         }
         const newPlus = $("<div class='plus'>");
         const newShortcut = $("<div class='shortcut'>");
         const newStationName = $("<div class='stationName'>").text(element.name);
         const newfm = $("<div class='fm'>").text(element.categories[0].ancestry);
         newParticStat.data("url", element.streams[0].stream);
-        console.log( element.streams[0].stream);
+        console.log(element.streams[0].stream);
         newExtend.prepend(newMinus);
         newExtend.append(newStationImage);
         newExtend.append(newPlus);
@@ -36,23 +77,38 @@ $(() => {
     const stations = $(".shortcut");
     let currentStation;
     stations.each(function(index, element) {
+        this.on=false;
         $(element).on('click', function() {
             audio.pause();
-            if (currentStation != undefined) {
-                currentStation.addClass("hidden");
-            }
             const parent = $(this).parent();
-            currentStation = parent.find(".extended");
-            const name = parent.find(".stationName").text();
-            currentStation.removeClass("hidden");
-             const container =$(".main");
-             container.animate({
-                 scrollTop: $(parent).offset().top - container.offset().top + container.scrollTop()-$(parent).height()/2
-             });
+            if(currentStation != undefined && this.on)
+            {
+                currentStation.addClass("hidden");
+                $(".currentStationName").text("");
+                audio.src = parent.data("");
+                console.log("powininienem wejść");
+                this.on=false;
+            }
+            else {
+                if(currentStation != undefined) {
+                    currentStation.addClass("hidden");
+                }
+                currentStation = parent.find(".extended");
+                const name = parent.find(".stationName").text();
+                currentStation.removeClass("hidden");
+                const container = $(".main");
+                container.animate({
+                    scrollTop: $(parent).offset().top - container.offset().top + container.scrollTop() - $(parent).height() / 2
+                });
 
-             audio.src=parent.data("url");
-             audio.play();
-            $(".currentStationName").text(name);
+                audio.src = parent.data("url");
+                this.on=true;
+                audio.play();
+                $(".currentStationName").text(name);
+                console.log(" nie powininienem wejść");
+
+            }
+
         })
     });
 
@@ -68,9 +124,8 @@ $(() => {
     $(".plus").each(function(index, element) {
         $(element).on('click', function() {
             console.log("plus");
-            if(audio.volume<1-epsilon)
-            {
-                audio.volume=audio.volume+0.1;
+            if (audio.volume < 1 - epsilon) {
+                audio.volume = audio.volume + 0.1;
                 console.log(audio.volume);
             }
         });
@@ -78,9 +133,8 @@ $(() => {
     //     //minus
     $(".minus").each(function(index, element) {
         $(element).on('click ', function() {
-            if(audio.volume-epsilon>0)
-            {
-                audio.volume=audio.volume-0.1;
+            if (audio.volume - epsilon > 0) {
+                audio.volume = audio.volume - 0.1;
                 console.log(audio.volume);
             }
         });
--- stationsName.js
+++ stationsName.js
@@ -1,7 +1,7 @@
 export default [
   {
     "id": 20604,
-    "name": "538NL",
+    "name": "3FM KX Radio",
     "country": "NL",
     "image": {
       "url": "",
@@ -9,8 +9,8 @@ export default [
         "url": "https://cdn.devality.com/station/20604/thumb_logo_radio538nl_640x300.jpg"
       }
     },
-    "slug": "538nl",
-    "website": "http://www.538.nl/",
+    "slug": "",
+    "website": "",
     "twitter": "",
     "facebook": "",
     "categories": [
@@ -35,7 +35,7 @@ export default [
   },
   {
     "id": 20603,
-    "name": "RTL Lounge Radio",
+    "name": "3 FM",
     "country": "NL",
     "image": {
       "url": "http://obrazki.joe.pl/sub_images/fajny-dziabong.gif",
@@ -43,8 +43,8 @@ export default [
         "url": "https://cdn.devality.com/station/20603/thumb_rtl-lounge-radio-logo.png"
       }
     },
-    "slug": "rtl-lounge-radio-95538006-3eb0-4140-ac04-71cbe080407e",
-    "website": "http://www.rtl.nl/rtl-lounge-radio/",
+    "slug": "",
+    "website": "",
     "twitter": "",
     "facebook": "",
     "total_listeners": 20,
@@ -52,7 +52,7 @@ export default [
       {
       "id": 5,
       "title": "Pop",
-      "description": "stations that normally play pop-music",
+      "description": "",
       "slug": "pop",
       "ancestry": null
       }
@@ -68,5 +68,107 @@ export default [
     ],
     "created_at": "2015-12-13T14:13:55+01:00",
     "updated_at": "2015-12-13T14:19:45+01:00"
-  }
+},
+{
+  "id": 20604,
+  "name": "538 Dance Radio",
+  "country": "NL",
+  "image": {
+    "url": "",
+    "thumb": {
+      "url": "https://cdn.devality.com/station/20604/thumb_logo_radio538nl_640x300.jpg"
+    }
+  },
+  "slug": "",
+  "website": "",
+  "twitter": "",
+  "facebook": "",
+  "categories": [
+    {
+      "id": 45,
+      "title": "Variety",
+      "description": "Variety or various, playing more or less everything.",
+      "slug": "variety",
+      "ancestry": "101"
+    }
+  ],
+  "streams": [
+    {
+      "stream": "http://playerservices.streamtheworld.com/api/livestream-redirect/TLPSTR10.mp3 ",
+      "bitrate": 128,
+      "content_type": "audio/mpeg",
+      "status": 1
+    }
+  ],
+  "created_at": "2015-12-13T14:19:14+01:00",
+  "updated_at": "2015-12-13T14:23:50+01:00"
+},
+{
+  "id": 20604,
+  "name": "Traffic Radio",
+  "country": "NL",
+  "image": {
+    "url": "",
+    "thumb": {
+      "url": "https://cdn.devality.com/station/20604/thumb_logo_radio538nl_640x300.jpg"
+    }
+  },
+  "slug": "",
+  "website": "",
+  "twitter": "",
+  "facebook": "",
+  "categories": [
+    {
+      "id": 45,
+      "title": "Variety",
+      "description": "Variety or various, playing more or less everything.",
+      "slug": "variety",
+      "ancestry": "55.7"
+    }
+  ],
+  "streams": [
+    {
+      "stream": "http://2.trafficstream.nl/traffic_main.mp3",
+      "bitrate": 128,
+      "content_type": "audio/mpeg",
+      "status": 1
+    }
+  ],
+  "created_at": "2015-12-13T14:19:14+01:00",
+  "updated_at": "2015-12-13T14:23:50+01:00"
+},
+{
+  "id": 20604,
+  "name": "FunX Latin",
+  "country": "NL",
+  "image": {
+    "url": "",
+    "thumb": {
+      "url": "https://cdn.devality.com/station/20604/thumb_logo_radio538nl_640x300.jpg"
+    }
+  },
+  "slug": "",
+  "website": "",
+  "twitter": "",
+  "facebook": "",
+  "categories": [
+    {
+      "id": 45,
+      "title": "Variety",
+      "description": "Variety or various, playing more or less everything.",
+      "slug": "variety",
+      "ancestry": "99"
+    }
+  ],
+  "streams": [
+    {
+      "stream": "http://icecast.omroep.nl/funx-latin-bb-mp3",
+      "bitrate": 128,
+      "content_type": "audio/mpeg",
+      "status": 1
+    }
+  ],
+  "created_at": "2015-12-13T14:19:14+01:00",
+  "updated_at": "2015-12-13T14:23:50+01:00"
+}
 ]
